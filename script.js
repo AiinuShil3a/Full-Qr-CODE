b2110f071d2d872534e083e37ac0c754f79ab103
AiinuShil3a <Shil3aiinu@outlook.com>
2023-01-26 19:07:09 +0700

    first commit

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-let btn = document.querySelector(".button");
-let qr_code_element = document.querySelector(".qr-code");
+function generateQRCode() {
+  let name = document.getElementById("name").value;
+  var radio = document.querySelector('input[name="time"]:checked'); 
 
-btn.addEventListener("click", () => {
-  let user_input = document.querySelector("#input_text");
-  if (user_input.value != "") {
-    if (qr_code_element.childElementCount == 0) {
-      generate(user_input);
+  if(radio) console.log(radio.value);
+
+  
+  if (name && radio) {
+      const data = { //ด้านหน้าคือหัวข้อที่อยู่ใน DB ด้านหลังคือค่าที่เราเชตไว้ดูด้านบน ^^^
+        name: name.padEnd(20), 
+        time: radio.value, 
+        
+
+      };
+      let qrcodeContainer = document.getElementById("qrcode");
+      qrcodeContainer.innerHTML = "";
+
+      new QRCode(qrcodeContainer, JSON.stringify(data));
+
+      
+      let download = document.createElement("button");
+      qrcodeContainer.appendChild(download);
+
+      let download_link = document.createElement("a");
+      download_link.setAttribute("download", "qr_code.png");
+      download_link.innerHTML = `Download <i class="fa-solid fa-download"></i>`;
+
+      download.appendChild(download_link);
+
+      let qr_code_img = document.getElementById("qrcode");
+      let qr_code_canvas = document.querySelector("canvas");
+
+      if (qr_code_img.getAttribute("src") == null) {
+        setTimeout(() => {
+          download_link.setAttribute("href", `${qr_code_canvas.toDataURL()}`);
+        }, 300);
+      } else {
+        setTimeout(() => {
+          download_link.setAttribute("href", `${qr_code_img.getAttribute("src")}`);
+        }, 300);
+      }
+
+      document.getElementById("qrcode-container").style.display = "block";
     } else {
-      qr_code_element.innerHTML = "";
-      generate(user_input);
+      alert("Please enter a valid URL");
     }
-  } else {
-    console.log("not valid input");
-    qr_code_element.style = "display: none";
-  }
-});
-
-function generate(user_input) {
-  qr_code_element.style = "";
-
-  var qrcode = new QRCode(qr_code_element, {
-    text: `${user_input.value}`,
-    width: 180, //128
-    height: 180,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H
-  });
-    let download = document.createElement("button");
-  qr_code_element.appendChild(download);
-
-  let download_link = document.createElement("a");
-  download_link.setAttribute("download", "qr_code.png");
-  download_link.innerHTML = `Download <i class="fa-solid fa-download"></i>`;
-
-  download.appendChild(download_link);
-
-  let qr_code_img = document.querySelector(".qr-code img");
-  let qr_code_canvas = document.querySelector("canvas");
-
-  if (qr_code_img.getAttribute("src") == null) {
-    setTimeout(() => {
-      download_link.setAttribute("href", `${qr_code_canvas.toDataURL()}`);
-    }, 300);
-  } else {
-    setTimeout(() => {
-      download_link.setAttribute("href", `${qr_code_img.getAttribute("src")}`);
-    }, 300);
-  }
 }
